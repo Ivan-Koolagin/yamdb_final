304b1ffd3150e3f04b69a6831f17024cbc9041cb
Кулагин Иван <ivkulagin@gmail.com>
2023-05-01 15:08:58 +0500

    add one

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,12 +1,8 @@
-from .views import (CategoryViewSet, CommentsViewSet,
-                    GenreViewSet, ReviewViewSet,
-                    TitleViewSet, UserViewSet,
-                    email_verifications, self_registration,
-                    )
+from .views import (CategoryViewSet, CommentsViewSet, GenreViewSet,
+                    ReviewViewSet, TitleViewSet, UserViewSet,
+                    email_verifications, self_registration)
 from django.urls import include, path
 from rest_framework import routers
-
-
 
 router_v1 = routers.DefaultRouter()
 router_v1.register("users", UserViewSet)
